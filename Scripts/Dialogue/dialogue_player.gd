853d9ba8825fb83146afd08b4a72231eb7657dd0
Alexander Axstrand <alexx.axstrand@gmail.com>
2024-11-13 17:23:42 +0000

    Dialogue Box

--- Scripts/Dialogue/dialogue_player.gd
+++ Scripts/Dialogue/dialogue_player.gd
@@ -6,8 +6,14 @@ var dialogue_lines = []
 var dialogue_playing = false
 
 
+@onready var label: Label = $TextureRect/Label
+@onready var texture_rect: TextureRect = $TextureRect
+@onready var type_writing_effect: AnimationPlayer = $TextureRect/Label/AnimationPlayer
+
+
 func _ready() -> void:
 	SignalBus.show_dialogue.connect(_on_show_dialogue)
+	texture_rect.visible = false
 
 
 func _on_show_dialogue(key, path):
@@ -21,7 +27,10 @@ func _on_show_dialogue(key, path):
 
 func show_current_line():
 	# Initiate dialogue interaction
-	print(dialogue_lines[current_line_index])
+	#print(dialogue_lines[current_line_index])
+	texture_rect.visible = true
+	label.text = dialogue_lines[current_line_index]
+	type_writing_effect.play("type_writing_effect")
 	dialogue_playing = true
 	
 	
@@ -34,9 +43,13 @@ func advance_dialogue():
 		dialogue_playing = false
 		# TODO
 		# Remove dialogue box sprite from screen
+		texture_rect.visible = false
 		current_line_index = 0 # Reset index for future interactions
 	else:
-		print(dialogue_lines[current_line_index])
+		#print(dialogue_lines[current_line_index])
+		texture_rect.visible = true
+		type_writing_effect.play("type_writing_effect")
+		label.text = dialogue_lines[current_line_index]
 
 func _input(event):
 	# While in dialogue,
